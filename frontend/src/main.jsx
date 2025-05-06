d3258b9cd1089cbd2aa14f7f968a70a06cae2a83
swapnilparicha <swapnilparicha@gmail.com>
2025-05-06 19:27:55 +0530

    deploy fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,18 +4,18 @@ import App from './App.jsx'
 import './index.css'
 
 // 1. Import both providers
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
-import Gleap from 'gleap'
+// import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
+// import Gleap from 'gleap'
 
 // 2. Initialize Gleap once, before you render
-Gleap.init({
-  apiKey: 'YOUR_GLEAP_PROJECT_API_KEY', 
-  pingInterval: Number(import.meta.env.VITE_GLEAP_PING_INTERVAL)
-})
+// Gleap.init({
+//   apiKey: 'YOUR_GLEAP_PROJECT_API_KEY', 
+//   pingInterval: Number(import.meta.env.VITE_GLEAP_PING_INTERVAL)
+// })
 
 // 3. Render your App wrapped in the ReCaptcha provider
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_GLEAP_RECAPTCHA_KEY}>
+  // <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_GLEAP_RECAPTCHA_KEY}>
     <App />
-  </GoogleReCaptchaProvider>
+  // </GoogleReCaptchaProvider>
 )
